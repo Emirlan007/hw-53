db74f4fdd6579b67430bbf4d2ddbf1e664eaf68b
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2024-12-27 14:23:16 +0600

    В основной компонент App добавлен интерфейс Task с id типа string, генерация уникального строкового id и логика удаления задач а также структура JSX для отображения списка задач и формы добавления.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import AddTaskForm from "./components/AddTaskForm";
+import Task from "./components/Task.tsx";
 
-const App = () => (
-    <>
+interface Task {
+  id: string;
+  text: string;
+}
 
-    </>
-);
+const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: "1", text: "Задача 1" },
+    { id: "2", text: "Задача 2" },
+    { id: "3", text: "Задача 3" },
+  ]);
 
-export default App
+  const handleAddTask = (taskText: string) => {
+    const newTask = {
+      id: Date.now().toString(),
+      text: taskText,
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const handleDeleteTask = (taskId: string) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  return (
+    <div>
+      <h1>ToDo</h1>
+      <AddTaskForm onAddTask={handleAddTask} />
+      <ol className="list">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            onDelete={handleDeleteTask}
+          />
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
